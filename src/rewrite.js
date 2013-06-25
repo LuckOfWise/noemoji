23a779faf28cf96df911a611305002a2716510b4
kazuhisa <ak.hisashi@gmail.com>
2013-06-25 14:32:10 +0900

    modified rewrite.js
    use jQuery.each

--- src/rewrite.js
+++ src/rewrite.js
@@ -1,10 +1,10 @@
 targets = new Array(":)", ":(",":D","8-)",":o",";)",";(","(sweat)",":|",":*",":p","(blush)",":^)","|-)","(inlove)","]:)","(talk)","(yawn)","(puke)","(emo)","8-|",":#)","(nod)","(shake)","(^^;)","(whew)","(clap)","(bow)","(roger)","(flex)","(dance)","(:/)","(devil)","(*)","(h)","(F)","(cracker)","(^)","(coffee)","(beer)","(handshake)","(y)");
 
-for (var i = 0; i < targets.length; i++) {
-	var target = "img[alt='" + targets[i] + "']";
+$.each(targets, function() {
+	var target = "img[alt='" + this + "']";
 	$(target).each(function() {
-	  var alt = $(this).attr('alt');
-	  $(this).after(alt);
-	  $(this).remove();
-	});	
-}
+	    var alt = $(this).attr('alt');
+	    $(this).after(alt);
+	    $(this).remove();
+	});
+});
